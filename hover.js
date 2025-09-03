8ff4eebb2f7a04d7f5067855e7cebed189382a84
Emil Puthur <emilsahel@gmail.com>
2025-09-02 22:27:56 -0400

    made mobile responsive

--- hover.js
+++ hover.js
@@ -2,11 +2,16 @@ nav = document.querySelector("#navbar");
 btn = document.querySelector("#ddsvg");
 logo = document.querySelector("#socials");
 
-nav.addEventListener("mouseover", () => {
-    btn.classList.add("-translate-y-100", "rotate-1000");
+if(isDesktop) {
+    nav.addEventListener("mouseover", () => {
+        btn.classList.add("-translate-y-100", "rotate-1000");
+        socials.classList.remove("-translate-y-100");
+    });
+    nav.addEventListener("mouseout", () => {
+        btn.classList.remove("-translate-y-100", "rotate-1000");
+        socials.classList.add("-translate-y-100");
+    });
+} else {
     socials.classList.remove("-translate-y-100");
-});
-nav.addEventListener("mouseout", () => {
-    btn.classList.remove("-translate-y-100", "rotate-1000");
-    socials.classList.add("-translate-y-100");
-});
+    btn.classList.add("-translate-y-100");
+}
--- scrollfx.js
+++ scrollfx.js
@@ -1,28 +1,27 @@
-const isDesktop = window.innerWidth >= 768;
+let isDesktop = false;window.innerWidth >= 768;
 
 const revealsR = document.querySelectorAll(".scrollfxr");
 const revealsL = document.querySelectorAll(".scrollfxl");
 
 if (isDesktop) {
-const observerR = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0", "translate-x-full");
-        } else {
-            entry.target.classList.add("opacity-0", "translate-x-full");
-        }
-    })
-});
-const observerL = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            entry.target.classList.remove("opacity-0", "-translate-x-full");
-        } else {
-            entry.target.classList.add("opacity-0", "-translate-x-full");
-        }
-    })
-});
+    const observerR = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("opacity-0", "translate-x-full");
+            } else {
+                entry.target.classList.add("opacity-0", "translate-x-full");
+            }
+        })
+    });
+    const observerL = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("opacity-0", "-translate-x-full");
+            } else {
+                entry.target.classList.add("opacity-0", "-translate-x-full");
+            }
+        })
+    });
+    revealsR.forEach(element => observerR.observe(element));
+    revealsL.forEach(element => observerL.observe(element));
 }
-
-revealsR.forEach(element => observerR.observe(element));
-revealsL.forEach(element => observerL.observe(element));
